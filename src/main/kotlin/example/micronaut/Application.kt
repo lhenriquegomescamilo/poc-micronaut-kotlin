34a16dbc283bc1704afe88674cd7c869a91de14a
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-28 17:17:41 -0300

    Using log

--- src/main/kotlin/example/micronaut/Application.kt
+++ src/main/kotlin/example/micronaut/Application.kt
@@ -1,14 +1,16 @@
 package example.micronaut
 
 import io.micronaut.runtime.Micronaut
+import org.slf4j.LoggerFactory
 
 object Application {
-
+    private val log = LoggerFactory.getLogger(Application.javaClass)
     @JvmStatic
     fun main(args: Array<String>) {
         Micronaut.build()
                 .packages("example.micronaut")
                 .mainClass(Application.javaClass)
                 .start()
+        log.info("The application is using ${Runtime.getRuntime().totalMemory() / 1024 / 1024} mb ")
     }
 }
